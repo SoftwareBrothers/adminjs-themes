c62566e84cf154e2586186d512fccedd2b7808c7
Arian Sobczak <arian.sobczak@rst.com.pl>
2022-03-11 12:49:58 +0100

    chore: added white logo to example app

--- example/src/admin/admin.ts
+++ example/src/admin/admin.ts
@@ -2,12 +2,19 @@ import AdminJSExpress from '@adminjs/express';
 import AdminJS, { AdminJSOptions } from 'adminjs';
 import { Express } from 'express';
 import path from 'path';
+import { DarkTheme } from '../../../src/themes';
 import { themes } from './themes';
 
-const PALETTE_PAGE = AdminJS.bundle(path.join(__dirname, './components/palette'));
+const PALETTE_PAGE = AdminJS.bundle(
+  path.join(__dirname, './components/palette')
+);
 
 export const adminConfig: AdminJSOptions = {
   availableBrandings: themes.map(theme => ({ theme })),
+  branding: {
+    theme: DarkTheme,
+    logo: '/logo_white.svg',
+  },
   pages: {
     Palette: {
       component: PALETTE_PAGE,
