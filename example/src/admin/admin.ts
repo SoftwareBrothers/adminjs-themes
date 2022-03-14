6e6d368e6bf1b3b9a31688bff9656861953656b3
Arian Sobczak <arian.sobczak@rst.com.pl>
2022-03-14 14:15:50 +0100

    chore: extended available brandings with logos

--- example/src/admin/admin.ts
+++ example/src/admin/admin.ts
@@ -2,15 +2,23 @@ import AdminJSExpress from '@adminjs/express';
 import AdminJS, { AdminJSOptions } from 'adminjs';
 import { Express } from 'express';
 import path from 'path';
-import { DarkTheme } from '../../../src/themes';
-import { themes } from './themes';
+import { DarkTheme, LightTheme } from '../../../src/themes';
 
 const PALETTE_PAGE = AdminJS.bundle(
   path.join(__dirname, './components/palette')
 );
 
 export const adminConfig: AdminJSOptions = {
-  availableBrandings: themes.map(theme => ({ theme })),
+  availableBrandings: [
+    {
+      theme: LightTheme,
+      logo: '/logo.svg',
+    },
+    {
+      theme: DarkTheme,
+      logo: '/logo_white.svg',
+    },
+  ],
   branding: {
     theme: DarkTheme,
     logo: '/logo_white.svg',
