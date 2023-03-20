06a53efe649a63e531f6d2f58385b5818d713b87
Arian Sobczak <arian.sobczak@rst.com.pl>
2023-03-20 14:05:20 +0100

    chore: draft dark theme

---
+++ src/themes/dark/components/SidebarBranding.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@adminjs/design-system';
+import { ViewHelpers } from 'adminjs';
+import React from 'react';
+import Logo from '../assets/logo';
+
+const h = new ViewHelpers()
+
+const SidebarBranding = () => (
+  <Box as="a" href={h.dashboardUrl()} flex alignItems="center" justifyContent="center" py="xl">
+    <Logo />
+  </Box>
+);
+
+export default SidebarBranding;
