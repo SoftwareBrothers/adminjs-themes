6aa045d1ea58d0ff9c5bb914f45bffc91ddeabc2
Arian Sobczak <arian.sobczak@rst.com.pl>
2023-03-31 11:58:30 +0200

    chore: improved cli documentations

--- src/cli/actions/generateTheme.ts
+++ src/cli/actions/generateTheme.ts
@@ -6,6 +6,15 @@ import kebabCase from 'lodash/kebabCase.js';
 const mkdir = util.promisify(fs.mkdir);
 const writeFile = util.promisify(fs.writeFile);
 
+/**
+  Generates a theme structure in the specified output directory.
+  @async
+  @function generateTheme
+  @param {string} name - The name of the theme to generate. Name is converted to theme ID in kebab case.
+  @param {string} description - The description of the theme.
+  @param {string} [output='./src/themes'] - The output directory where the theme will be generated.
+  @returns {Promise<void>} - A promise that resolves when the theme generation is complete.
+*/
 export const generateTheme = async (
   name: string,
   description,
@@ -14,6 +23,11 @@ export const generateTheme = async (
   const themeId = kebabCase(name);
   const themeDir = path.resolve(output, themeId);
   const componentsDir = path.resolve(themeDir, 'components');
+
+  if (fs.existsSync(themeDir)) {
+    console.error(`⚠️ Theme ${name} already exist ${themeDir}`);
+    process.exit(1);
+  }
 
   await mkdir(componentsDir, { recursive: true });
   await mkdir(themeDir, { recursive: true });
@@ -42,4 +56,6 @@ export const generateTheme = async (
       `};`,
     ].join('\n')
   );
+
+  console.log(`✔️ Generated files for ${themeId} theme in ${themeDir}`);
 };
--- src/cli/generate.ts
+++ src/cli/generate.ts
@@ -4,9 +4,16 @@ import { generateTheme } from './actions/generateTheme.js';
 
 export const generate = createCommand('generate')
   .description('New theme generator')
-  .argument('<name>', 'Name of theme')
-  .option('--description <string>', 'Theme description', undefined)
-  .option('--output <directory>', 'Output directory', './src/themes')
+  .argument(
+    '<name>',
+    'The name of the theme to generate. Name is converted to theme ID in kebab case.'
+  )
+  .option('--description <string>', 'The description of the theme.', undefined)
+  .option(
+    '--output <directory>',
+    'The output directory where the theme will be generated.',
+    './src/themes'
+  )
   .action(async (name, options) => {
     try {
       await generateTheme(name, options.description, options.output);
