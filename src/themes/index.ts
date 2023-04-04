c4fc9bccafb87fb115595a16404dcb6c6fddfa63
Arian Sobczak <arian.sobczak@rst.com.pl>
2023-04-04 12:13:15 +0200

    chore: renamed wide theme export

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,3 +1,3 @@
 export { themeConfig as dark } from './dark/index.js';
-export { themeConfig as wide } from './no-sidebar/index.js';
+export { themeConfig as noSidebar } from './no-sidebar/index.js';
 export { themeConfig as light } from './light/index.js';
